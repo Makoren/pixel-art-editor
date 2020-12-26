4591189b52541cd03bda122770b4b893d82fa49b
Luke Lazzaro <makorenwastaken@gmail.com>
2020-12-27 10:03:45 +1100

    Create canvas grid

--- pixel-art-editor/ViewController.swift
+++ pixel-art-editor/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let skView = self.view as? SKView,
+              let scene = SKScene(fileNamed: "Canvas") else {
+            fatalError("what")
+        }
+        
+        scene.scaleMode = .aspectFill
+        
+        let grid = drawGridLines()
+        grid.position = CGPoint(x: -100, y: -100)   // use a camera to move around the scene instead of this
+        scene.addChild(grid)
+        
+        skView.presentScene(scene)
     }
 
+    func drawGridLines() -> SKShapeNode {
+        let gridWidth = 8
+        let gridHeight = 6
+        let cellSize = 24
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addRect(CGRect(x: 0, y: 0, width: cellSize * gridWidth, height: cellSize * gridHeight))
+        
+        for row in 1 ..< gridHeight {
+            path.move(to: CGPoint(x: 0, y: row * cellSize))
+            path.addLine(to: CGPoint(x: cellSize * gridWidth, y: row * cellSize))
+        }
+        
+        for col in 1 ..< gridWidth {
+            path.move(to: CGPoint(x: col * cellSize, y: 0))
+            path.addLine(to: CGPoint(x: col * cellSize, y: cellSize * gridHeight))
+        }
+        
+        let shape = SKShapeNode()
+        shape.path = path
+        return shape
+    }
 
 }
 
