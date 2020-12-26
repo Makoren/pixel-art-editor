e33e4f9ed719ead85de1dd7de774730e4fca6213
Luke Lazzaro <makorenwastaken@gmail.com>
2020-12-27 10:55:38 +1100

    Redo Canvas class so it can use UIResponder methods

---
+++ pixel-art-editor/Canvas.swift
@@ -0,0 +1,60 @@
+//
+//  Canvas.swift
+//  pixel-art-editor
+//
+//  Created by Luke Lazzaro on 27/12/20.
+//
+
+import SpriteKit
+
+class Canvas: SKView {
+
+    let skScene: SKScene
+    let grid: SKShapeNode
+    
+    required init?(coder: NSCoder) {
+        guard let newScene = SKScene(fileNamed: "Canvas") else {
+            fatalError("Could not load canvas!")
+        }
+        skScene = newScene
+        grid = SKShapeNode()
+        
+        // initialize properties before this
+        super.init(coder: coder)
+        
+        skScene.scaleMode = .aspectFill
+        
+        grid.path = drawGridLines()
+        skScene.addChild(grid)
+        
+        presentScene(skScene)
+    }
+    
+    func drawGridLines() -> CGPath {
+        let gridWidth = 8
+        let gridHeight = 6
+        let cellSize = 24
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addRect(CGRect(x: 0, y: 0, width: cellSize * gridWidth, height: cellSize * gridHeight))
+        
+        for row in 1 ..< gridHeight {
+            path.move(to: CGPoint(x: 0, y: row * cellSize))
+            path.addLine(to: CGPoint(x: cellSize * gridWidth, y: row * cellSize))
+        }
+        
+        for col in 1 ..< gridWidth {
+            path.move(to: CGPoint(x: col * cellSize, y: 0))
+            path.addLine(to: CGPoint(x: col * cellSize, y: cellSize * gridHeight))
+        }
+        
+        return path
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        print(touch.location(in: grid))
+    }
+    
+}
--- pixel-art-editor/ViewController.swift
+++ pixel-art-editor/ViewController.swift
@@ -13,13 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let skView = self.view as? SKView else {
-            fatalError("Could not load canvas")
-        }
-        
-        let canvas = Canvas()
-        canvas.scene.scaleMode = .aspectFill
-        skView.presentScene(canvas.scene)
     }
 
 }
