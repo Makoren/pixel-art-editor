9b972b24f4cfd59cc7fa0de484507fcae9139205
Luke Lazzaro <makorenwastaken@gmail.com>
2020-12-28 13:56:09 +1100

    Exporting to an image works and I'm not sure why

--- pixel-art-editor/ViewController.swift
+++ pixel-art-editor/ViewController.swift
@@ -10,10 +10,34 @@ import SpriteKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var canvasView: Canvas!
+    @IBOutlet weak var testImageView: UIImageView!
+    
+    @IBAction func exportButtonPressed(_ sender: Any) {
+        // create CGContext somehow
+        // loop over canvas children to get each shape node and stitch them together in the context
+        // turn the context into an image
+        // convert the image into PNG data and write it to a file
         
+        let width = CGFloat(canvasView.gridWidth * canvasView.cellSize)
+        let height = CGFloat(canvasView.gridHeight * canvasView.cellSize)
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        
+        let img = renderer.image { ctx in
+            for node in canvasView.canvasNode.children {
+                let shapeNode = node as! SKShapeNode
+                
+                let renderer2 = UIGraphicsImageRenderer(size: CGSize(width: CGFloat(canvasView.cellSize), height: CGFloat(canvasView.cellSize)))
+                let shapeImage = renderer2.image { ctx2 in
+                    ctx.cgContext.setFillColor(shapeNode.fillColor.cgColor)
+                    ctx.cgContext.fill(CGRect(x: shapeNode.position.x, y: shapeNode.position.y, width: CGFloat(canvasView.cellSize), height: CGFloat(canvasView.cellSize)))
+                }
+                print(shapeNode.position)
+                shapeImage.draw(at: CGPoint())
+            }
+        }
+        testImageView.image = img
     }
-
+    
 }
 
