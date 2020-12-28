0a0c13638b464f04a1c241260e4441deeb5de01b
Luke Lazzaro <makorenwastaken@gmail.com>
2020-12-28 16:25:21 +1100

    Final change for the day I promise

--- pixel-art-editor/Controllers/ViewController.swift
+++ pixel-art-editor/Controllers/ViewController.swift
@@ -14,18 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var testImageView: UIImageView!
     
     @IBAction func exportButtonPressed(_ sender: Any) {
-        let width = CGFloat(canvasView.gridWidth * canvasView.cellSize)
-        let height = CGFloat(canvasView.gridHeight * canvasView.cellSize)
+        let width = CGFloat(canvasView.gridWidth * 24)
+        let height = CGFloat(canvasView.gridHeight * 24)
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
         
-        let data = renderer.pngData { ctx in
+        let img = renderer.image { ctx in
             for node in canvasView.canvasNode.children {
                 let shapeNode = node as! SKShapeNode
                 ctx.cgContext.setFillColor(shapeNode.fillColor.cgColor)
                 ctx.cgContext.setStrokeColor(UIColor.clear.cgColor)
-                ctx.cgContext.fill(CGRect(x: shapeNode.position.x, y: shapeNode.position.y, width: CGFloat(canvasView.cellSize), height: CGFloat(canvasView.cellSize)))
+                ctx.cgContext.fill(CGRect(x: shapeNode.position.x, y: shapeNode.position.y, width: 24, height: 24))
             }
         }
+        
+        // closest I can get to fixing orientation
+        let newImage = UIImage(cgImage: img.cgImage!, scale: 1, orientation: .downMirrored)
+        let data = UIGraphicsImageRenderer(size: CGSize(width: width, height: height)).pngData { _ in newImage.draw(at: .zero) }
         
         let filename = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("test.png")
         print(filename)
